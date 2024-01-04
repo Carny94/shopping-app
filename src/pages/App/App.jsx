44e148fd872eab2333a2ae5dbed127ca682c1364
mookdook94 <134980150+mookdook94@users.noreply.github.com>
2024-01-03 20:09:13 -0800

    added navbar to view only if user

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,8 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import { useState } from 'react';
+import NavBar from '../../components/NavBar/NavBar';
 import AuthPage from '../AuthPage/Authpage';
 import HomePage from '../HomePage/HomePage';
+import ServicesPage from '../ServicesPage/ServicesPage';
+
 
 
 
@@ -12,10 +15,13 @@ export default function App() {
   return (
     <main className="App">
       {user ?
+      <>
+      <NavBar />
       <Routes>
         <Route path="/" element= {<HomePage />} />
-
+        <Route path="/services" element= {<ServicesPage />} />
       </Routes>
+      </>
       :
       <AuthPage />
        
