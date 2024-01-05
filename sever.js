51017ad94e93cac4740261c7e344f966c715919b
mookdook94 <134980150+mookdook94@users.noreply.github.com>
2024-01-04 17:41:57 -0800

    finishing login form

--- sever.js
+++ sever.js
@@ -17,12 +17,12 @@ require('./config/database');
  // to serve from the production 'build' folder
  app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
  app.use(express.static(path.join(__dirname, 'build')));
-
+ const port = process.env.PORT || 3001;
  // Put API routes here, before the "catch all" route
+ app.use('/api/users', require('./routes/api/users'));
 
 // The following "catch all" route (note the *) is necessary
-// to return the index.html on all non-AJAX requests
-const port = process.env.PORT || 3001;
+
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
